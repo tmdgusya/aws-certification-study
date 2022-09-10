15b168512f96fd1440dad90f50179e1aaaf46f6f
tmdgusya <tmdgusya@naver.com>
2022-09-10 23:14:08 +0900

    add bootstrap script

---
+++ ec2-user-data.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# This Script is bootstrap script for ec2 instance
+
+yum update -y
+yum install -y httpd
+systemctl start httpd
+systemctl enable httpd
+echo "<h1>Hello World from $(hostname -f)</h1>" > /var/www/html/index.html
